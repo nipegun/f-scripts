50ad50e7f4196469cfdb0d3df71168fbe4dee6b4
NiPeGun <nipegun@gmail.com>
2020-12-15 01:43:39 +0100

    Update GetBINsFromFirmwareImage.sh

--- 6660cable/GetBINsFromFirmwareImage.sh
+++ 6660cable/GetBINsFromFirmwareImage.sh
@@ -22,7 +22,7 @@ ColorEnd='\033[0m'
 apt-get -y update > /dev/null
 apt-get -y install dialog > /dev/null
 
-menu=(dialog --timeout 5 --checklist "FritzBox 6660 cable, firmware version (SpaceBar to mark, Enter to proceed):" 22 76 16)
+menu=(dialog --timeout 5 --checklist "FritzBox 6660 cable, firmware selection (SpaceBar to mark, Enter to proceed):" 22 86 16)
   options=(1 "7.23 Deustchland" off
            2 "7.23 International" off)
   choices=$("${menu[@]}" "${options[@]}" 2>&1 >/dev/tty)
