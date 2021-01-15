5e4562c46275d17552e75649cd66cb977c89d787
NiPeGun <nipegun@gmail.com>
2021-01-15 10:31:25 +0100

    Create EVA-Set-Vendor-Retail.ps1

---
+++ 6660cable/EVA-Set-Vendor-Retail.ps1
@@ -0,0 +1,23 @@
+EVA-Discover.ps1
+
+Write-Output ""
+Write-Output "Value of the DMC environment variable is:"
+Write-Output ""
+EVA-FTP-Client.ps1 -Verbose -ScriptBlock { GetEnvironmentValue DMC }
+
+Write-Output ""
+Write-Output "Setting value to Retail (RTL=y,SL1):"
+Write-Output ""
+EVA-FTP-Client.ps1 -Verbose -ScriptBlock { SetEnvironmentValue DMC "RTL=y,SL1"}
+
+Write-Output ""
+Write-Output "Now value is:"
+Write-Output ""
+EVA-FTP-Client.ps1 -Verbose -ScriptBlock { GetEnvironmentValue DMC }
+
+Write-Output ""
+Write-Output "If you want to reboot now, run:"
+Write-Output "EVA-Reboot.ps1"
+Write-Output "or wait a minute or two and the Fritz!Box will reboot by itself"
+Write-Output ""
+
